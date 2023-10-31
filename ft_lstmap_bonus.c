df040adf7ad11cd1cfe47dfe7385fbd48ea22d3f
Mauricio Rodriguez <maurodri@student.42sp.org.br>
2023-10-31 19:32:43 -0300

    fix lstmap memory

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: maurodri <maurodri@student.42sp...>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 15:03:44 by maurodri          #+#    #+#             */
-/*   Updated: 2023/10/24 15:03:44 by maurodri         ###   ########.fr       */
+/*   Updated: 2023/10/31 19:30:52 by maurodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,27 @@ t_list	*ft_lstmap(t_list *lst, void *(*fun)(void *), void (*del)(void *))
 	t_list	*new_lst;
 	t_list	*ptr;
 	t_list	*temp;
+	void	*content;
 
 	if (!lst || !fun || !del)
 		return (NULL);
-	new_lst = ft_lstnew(fun(lst->content));
+	content = fun(lst->content);
+	new_lst = ft_lstnew(content);
 	if (!new_lst)
+	{
+		del(content);
 		return (NULL);
+	}
 	ptr = new_lst;
 	lst = lst->next;
 	while (lst)
 	{
-		temp = ft_lstnew(fun(lst->content));
+		content = fun(lst->content);
+		temp = ft_lstnew(content);
 		if (!temp)
 		{
-			ft_lstclear(&lst, del);
+			del(content);
+			ft_lstclear(&new_lst, del);
 			return (NULL);
 		}
 		ptr->next = temp;
